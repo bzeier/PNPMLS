f71cc75dd574ec37a55c19984192b5e830afeab9
Brandon Zeier <43791891+bzeier@users.noreply.github.com>
2022-09-09 17:10:35 +0200

    Add new c++ class PlayerState

---
+++ Public/PlayerState_MLS.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "GameInterface.h"
+#include "PlayerState_MLS.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PNPMLS_API APlayerState_MLS : public APlayerState, public IGameInterface
+{
+	GENERATED_BODY()
+	
+};
