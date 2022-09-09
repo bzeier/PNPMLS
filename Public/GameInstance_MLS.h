b0af37423a8ba0cad2c3f1ec191a520f2184b376
Brandon Zeier <43791891+bzeier@users.noreply.github.com>
2022-09-09 17:15:33 +0200

    add new c++ class GameInstance_MLS

---
+++ Public/GameInstance_MLS.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "GameInstance_MLS.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PNPMLS_API UGameInstance_MLS : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
