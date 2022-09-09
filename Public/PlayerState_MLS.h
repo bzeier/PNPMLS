49f08f07b95f79054c3cface3e5e2be730afd382
Brandon Zeier <43791891+bzeier@users.noreply.github.com>
2022-09-09 17:33:10 +0200

    Add playerState_MLS interface events

--- Public/PlayerState_MLS.h
+++ Public/PlayerState_MLS.h
@@ -14,5 +14,29 @@ UCLASS()
 class PNPMLS_API APlayerState_MLS : public APlayerState, public IGameInterface
 {
 	GENERATED_BODY()
-	
+
+
+public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+		void ShowPlayerAlert(const FString& message);
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+		void SetDisconnectReason(const FString& reason);
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+		void SpawnGamePawn(UClass* GamePawnClass, FTransform Transform, APlayerController* PlayerController);
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+		void ReadyUp();
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+		void AddKill();
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+		void AddDeath();
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+		void SetPlayerScore(int score);
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+		void AddKillfeedMessage(APlayerState* Killed, APlayerState* Killer);
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+		void CreateWinLooseWidget(bool win);
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+		void SetMatchPlaced(int Placed);
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+		void SetMatchConlusion();
 };
